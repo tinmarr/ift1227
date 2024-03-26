405ad7f7795b29d452053675679595ffd415e059
Martin Chaperot <martin.chapino@gmail.com>
2024-03-26 16:02:31 -0400

    Devoir 3 fini!

--- devoir3/carreMagique.asm
+++ devoir3/carreMagique.asm
@@ -1,10 +1,15 @@
+# Carré Magique
+# Demande l'entrée d'un carré 4x4 puis te dis si c'est un carré magique.
+# Date: 26 mars 2024
+# Auteurs: Martin Chaperot (20205638) et Hamza Ali Ousalah (20249230)
+# Addresse Courriel: martin.chaperot@umontreal.ca et hamza.ali.ousalah@umontreal.ca
 .data
 entered: .asciiz "La donnée est déjà entrée! Entrez une autre valeur de 1 à 16.\n"
 notValid: .asciiz "Le nombre entré n'est pas valide. Entrez une valeur de 1 à 16.\n"
 tabChar: .asciiz "\t"
 newLineChar: .asciiz "\n"
-magique: .asciiz "La matrice n'est pas un carré magique!"
-pasMagique: .ascizz "Carré magique !!! La valeur magique est: "
+pasMagique: .asciiz "La matrice n'est pas un carré magique!"
+magique: .asciiz "Carré magique !!! La valeur magique est: "
 verif: .word 0, 0, 0, 0 # tableau de verification de l'entrer (16 bytes)
 .text
 main: 
@@ -17,6 +22,27 @@ main:
 	li $a1, 4
 	li $a2, 4
 	jal afficherMat
+	
+	lui $a0, 0x1004
+	li $a1, 4
+	li $a2, 4
+	jal estMagique
+	move $t1, $v0
+	# print magique
+	li $v0, 4
+	la $a0, magique
+	syscall
+	# print number
+	li $v0, 1
+	move $a0, $t1
+	syscall
+	# exit
+	li $v0, 10
+	syscall
+	# pas magique
+	pasMagiqueConfirm: li $v0, 4
+	la $a0, pasMagique
+	syscall
 	# exit cleanly
 	li $v0, 10
 	syscall
@@ -99,8 +125,110 @@ afficherMat:
 		pNReturnTo: beq $t3, $t1, affichReturn
 		j afficherLoop
 	affichReturn: jr $ra
-sumRow: # sumRow(matAddr, rowNumber)
-sumCol: # sumCol(matAddr, colNumber)
-sumDiagLR: # sumDiagLR(matAddr)
-sumDiagRL: # sumDiagRL(matAddr)
-estMagique:
+sumRow: # sumRow(matAddr, nRows, nCols, rowNumber)
+	li $v0, 0 # sum
+	li $t0, 0 # j
+	srloop:
+		# offset calc
+		mul $t1, $a3, $a1
+		add $t1, $t1, $t0
+		sll $t1, $t1, 2
+		add $t1, $t1, $a0 # addr of number
+		lw $t1, 0($t1) # number
+		add $v0, $v0, $t1
+		addi $t0, $t0, 1
+		bne $t0, $a2, srloop
+	jr $ra
+sumCol: # sumCol(matAddr, nRows, nCols, colNumber)
+	li $v0, 0 # sum
+	li $t0, 0 # i
+	scloop:
+		# offset calc
+		mul $t1, $t0, $a1
+		add $t1, $t1, $a3
+		sll $t1, $t1, 2
+		add $t1, $t1, $a0 # addr of number
+		lw $t1, 0($t1) # number
+		add $v0, $v0, $t1
+		addi $t0, $t0, 1
+		bne $t0, $a1, scloop
+	jr $ra
+sumDiagLR: # sumDiagLR(matAddr, nRows, nCols)
+	li $v0, 0 # sum
+	li $t0, 0 # i
+	li $t1, 0 # j
+	sdlrLoop:
+		# offset calc
+		mul $t2, $t0, $a2
+		add $t2, $t2, $t1
+		sll $t2, $t2, 2
+		add $t2, $a0, $t2 # addr of num
+		lw $t2, 0($t2) # num
+		add $v0, $v0, $t2
+		addi $t0, $t0, 1
+		addi $t1, $t1, 1
+		bne $t0, $a1, sdlrLoop
+		bne $t1, $a2, sdlrLoop
+	jr $ra
+sumDiagRL: # sumDiagRL(matAddr, nRows, nCols)
+	li $v0, 0 # sum
+	li $t0, 0 # i
+	addi $t1, $a2, -1 # j
+	sdrlLoop:
+		# offset calc
+		mul $t2, $t0, $a2
+		add $t2, $t2, $t1
+		sll $t2, $t2, 2
+		add $t2, $a0, $t2 # addr of num
+		lw $t2, 0($t2) # num
+		add $v0, $v0, $t2
+		addi $t0, $t0, 1
+		addi $t1, $t1, -1
+		bne $t0, $a1, sdrlLoop
+		bne $t1, -1, sdrlLoop
+	jr $ra
+sumCoin: # sumCoin(matAddr, nRows, nCols)
+	li $v0, 0 # sum
+	# top left 
+	lw $t1, 0($a0)
+	add $v0, $v0, $t1
+	# top right
+	add $t1, $a1, -1
+	sll $t1, $t1, 2
+	add $t1, $a0, $t1
+	lw $t1, 0($t1)
+	add $v0, $v0, $t1
+	# bottom right
+	mul $t1, $a1, $a2
+	addi $t1, $t1, -1
+	sll $t2, $t1, 2
+	add $t2, $a0, $t2
+	lw $t2, 0($t2)
+	add $v0, $v0, $t2
+	# bottom left
+	addi $t2, $a2, -1
+	sub $t1, $t1, $t2
+	sll $t1, $t1, 2
+	add $t1, $a0, $t1
+	lw $t1, 0($t1)
+	add $v0, $v0, $t1
+	# return
+	jr $ra
+estMagique: # estMagique(matAddr, nRows, nCols)
+	move $t9, $ra
+	jal sumCoin
+	move $t8, $v0
+	jal sumDiagLR
+	bne $v0, $t8, pasMagiqueConfirm
+	jal sumDiagRL
+	bne $v0, $t8, pasMagiqueConfirm
+	li $a3, 0
+	rowColCalcLoop:
+		jal sumRow
+		bne $v0, $t8, pasMagiqueConfirm
+		jal sumCol
+		bne $v0, $t8, pasMagiqueConfirm
+		addi $a3, $a3, 1
+		bne $a3, $a1, rowColCalcLoop
+		bne $a3, $a2, rowColCalcLoop
+	jr $t9
